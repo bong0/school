87aea8eeb93a82c8cd9949d44f7635bcbb275c8f
Julian <julian@juliankessel.de>
2011-06-16 08:17:17 -0700

    fixed error in last commit due to soft linking

--- find_e.c
+++ find_e.c
@@ -1,1 +1,12 @@
-../../bsgg/find_e.c
+#include <stdio.h>
+/* a small exercise to understand passing command line arguments and interpret them */
+
+int main(int argc, const char *argv[]){
+ int ec=0;
+	for(int i=1; i<argc; i++){
+		for(int b=0; argv[i][b]!=0; b++){
+			if(argv[i][b] == 'e') ec++;
+		}
+	}
+	printf("es: %d\n",ec);
+}
