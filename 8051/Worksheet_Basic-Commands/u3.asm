70b67e9f0352568cb18a10d71bc5aeef608bbc84
bongo <bongo@bird.localdomain>
2012-05-17 12:27:26 +0200

    bugfixes; added new tasks; simplified code u9.asm

--- 8051/Worksheet_Basic-Commands/u3.asm
+++ 8051/Worksheet_Basic-Commands/u3.asm
@@ -3,7 +3,7 @@
 mov A, #080H; leftmost bit (light) is set
 mov P3, A; output on port3
 
-START:R0
+START:
   RR A; shift set bit one bit right
   mov P3, A; push result on port
 SJMP START
--- 8051/Worksheet_Basic-Commands/u9.asm
+++ 8051/Worksheet_Basic-Commands/u9.asm
@@ -1,19 +1,18 @@
-;U9: determine size of num and react
+;U9: determine size of num on port 2 and react
 PINPUT EQU P2
 POUTPUT EQU P3
-R0_MAN EQU 00H
+R0_MAN EQU 00H; workaround: we may not use R0, only dadr in CJNE
 
-mov A,#42 ; our counter
-mov R0,PINPUT
+mov A,PINPUT
+mov POUTPUT, #00H ; default: num is not greater than 42
 
-ITER:
-CJNE A,R0_MAN,DECR; if A!=PINPUT => decrement
-SJMP ISNG; else contents of A and R0 are equal
-DECR:
-DJNZ ACC,ITER; the counter has not reached zero, increment 
-SJMP END; reached zero => bail out
+CJNE A,#42,COMPARE; A equals 42 => compare CY
 
-ISNG:; being here indicates R0 <= start val of A
+COMPARE:
+JNC SETFLAG; A > 42
+JC END; else number is < 42
+
+SETFLAG:
 SETB POUTPUT.1; flag R0 <= 42
 
 END:
